1d218dcc360efd0fddc5cf01dd7ecdad19a4cbd1
aidan <aidanbarber88@hotmail.com>
2022-02-20 03:47:56 -0500

    tutorial 6

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,20 @@
 <script>
-	let firstName = 'Jimi';
-	let lastName = 'Hendrix';
-	let beltColour = 'black';
-
-	$: fullName = `${firstName} ${lastName}`;
-	$: {
-		console.log(beltColour);
-		console.log(fullName);
-	}
-
-	const handleClick = () =>{
-		beltColour = 'orange'
-	};
-
-	const handleInput = (e) =>{
-		beltColour = e.target.value;
-	};
+	let people = [
+		{ name: 'yoshi', beltColour: 'black', age: 25, id: 1 },
+		{ name: 'mario', beltColour: 'orange', age: 45, id: 2 },
+		{ name: 'luigi', beltColour: 'brown', age: 55, id: 3 },
+	]
 </script>
 
 <main>
-	<p style="color: {beltColour}">{fullName} - {beltColour} belt</p>
-	<!-- <button on:click={handleClick}>update belt colour</button> -->
-	<input type="text" bind:value={firstName}>
-	<input type="text" bind:value={lastName}>
-	<!-- <input type = "text" on:input={handleInput} value={belt}> -->
-	<input type="text" bind:value={beltColour}>
+	{#each people as person (person.id)}
+	<div>
+		<h4>{person.name}</h4>
+		<p>{person.age} years old, {person.beltColour} belt.</p>
+	</div>
+	{:else}
+	<p>There are no people to show...</p>
+	{/each}
 </main>
 
 <style>
@@ -35,12 +25,6 @@
 		margin: 0 auto;
 	}
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
 
 	@media (min-width: 640px) {
 		main {
